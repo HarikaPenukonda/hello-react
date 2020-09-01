75440fb2dd1603ae01eb0bab34018317ae12af31
harika penukonda <penukondaharika@gmail.com>
2020-09-01 13:49:10 -0500

    Rendering multiple react elements

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 
 
 ReactDOM.render(
-    React.createElement("h1",{style: {color:"red"}},"Hello!"),
+    React.createElement(
+      "ul",
+      {style: {color:"red"}},
+      React.createElement("li",null,"Hot Dogs"),
+      React.createElement("li",null,"Hamburgers"),
+      React.createElement("li",null,"Pizza"),
+      React.createElement("li",null,"Shushi")
+      ),
     document.getElementById('root')
   );
 
