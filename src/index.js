2d8ce285a518ea1d4d23d645edeafed50701b5fa
harika penukonda <penukondaharika@gmail.com>
2020-09-03 15:32:16 -0500

    Adding CSS to the text

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = "Madrid";
+let city = {
+  name : "Madrid",
+  country : "Spain"
+};
 
 ReactDOM.render(
-<h1>Hello from {city}</h1>,
+<h1 id="heading" className="cool-text">{city.name} is in {city.country}</h1>,
     document.getElementById('root')
   );
 
