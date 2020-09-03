8766746f83b4f9975cb81cbd94ac35f76379bca2
harika penukonda <penukondaharika@gmail.com>
2020-09-03 15:20:26 -0500

    JSX BASICS

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let city = "Madrid";
 
 ReactDOM.render(
-    React.createElement(
-      "ul",
-      {style: {color:"red"}},
-      React.createElement("li",null,"Hot Dogs"),
-      React.createElement("li",null,"Hamburgers"),
-      React.createElement("li",null,"Pizza"),
-      React.createElement("li",null,"Shushi")
-      ),
+<h1>Hello from {city}</h1>,
     document.getElementById('root')
   );
 
